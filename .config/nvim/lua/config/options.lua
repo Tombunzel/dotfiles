128f1db8b817f6313341ecdfb3c2b850ebe31413
tombunzel <bunzel.tom@gmail.com>
2026-02-02 10:24:28 +0100

    feat(options.lua): add python provider

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -32,6 +32,10 @@ vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
 
+-- Python3
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+vim.g.loaded_python3_provider = nil
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
